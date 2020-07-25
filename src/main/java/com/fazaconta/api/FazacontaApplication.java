cd192b4540e7f093358b56527a28688211ae1ab8
Michel de Oliveira <michel.cota@linkcom.com.br>
2020-07-25 17:47:14 -0300

    Initial commit

---
+++ src/main/java/com/fazaconta/api/FazacontaApplication.java
@@ -0,0 +1,13 @@
+package com.fazaconta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazacontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazacontaApplication.class, args);
+	}
+
+}
