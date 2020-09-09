0ebdb5f66907fa4445b692d8821b2099148cf513
Michel de Oliveira <michel.cota@linkcom.com.br>
2020-09-08 22:00:17 -0300

    enable cors

--- src/main/java/com/fazaconta/api/controller/ProductController.java
+++ src/main/java/com/fazaconta/api/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.fazaconta.api.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +16,10 @@ import com.fazaconta.api.error.ProductNotFoundException;
 import com.fazaconta.api.model.Product;
 
 @RestController
+@CrossOrigin(origins="*", allowedHeaders = "*")
 public class ProductController {
 	
-	private final ProductRepository productRepository;
+	private ProductRepository productRepository;
 	
 	public ProductController(ProductRepository productRepository) {
 		this.productRepository = productRepository;
