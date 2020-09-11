5b27250c32984a75dc20d5e3daf4dcfe64ed252b
Michel de Oliveira <michel.cota@linkcom.com.br>
2020-09-11 02:31:20 -0300

    endpoint created to search product by name

--- src/main/java/com/fazaconta/api/data/ProductRepository.java
+++ src/main/java/com/fazaconta/api/data/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.fazaconta.api.data;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.fazaconta.api.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query("SELECT * FROM Product WHERE name LIKE :name%")
+	public Page<Product> findProductsByName(@Param("name") String name);
 }
